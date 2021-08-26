149437355c50e23a27d3f2541b1c2c6c869fded7
Merry <amo168@gmail.com>
2021-08-26 03:01:10 +0200

    WIP: Localization

--- src/features/constatations/routes/Create.tsx
+++ src/features/constatations/routes/Create.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Card } from "react-native-elements/dist/card/Card";
 import { ImagesPart } from "../subfeatures/images/components/ImagesPart";
 
-import { LocationPart } from "../subfeatures/location/components/LocalizationForm";
+import { LocalizationPart } from "../subfeatures/location/components/LocalizationPart";
 import { useCreateConstatation } from "../hooks/useCreateConstatation";
 
 export function Create() {
--- src/features/constatations/subfeatures/images/components/ImagesPartView.tsx
+++ src/features/constatations/subfeatures/images/components/ImagesPartView.tsx
@@ -2,15 +2,10 @@ import React, { useState, useEffect } from "react";
 import { Card, Button, Icon, Text, Input } from "react-native-elements";
 import { Image, View, Platform, ScrollView } from "react-native";
 import * as ImagePicker from "expo-image-picker";
-<<<<<<< HEAD:src/features/constatations/subfeatures/images/components/ImagesPartView.tsx
 import { useUploadImage } from "../hooks/useUploadImage";
 import { useDeleteImage } from "../hooks/useDeleteImage";
 import { useImage } from "../hooks/useImage";
 
-=======
-import { useUploadImage } from "../../hooks/parts/images/useUploadImage";
-import { useDeleteImage } from "../../hooks/parts/images/useDeleteImage";
->>>>>>> ba2eafa75f8d6794619496d6b4b587a224df979b:src/features/constatations/components/PartsView/ImagesPartView.tsx
 
 type ImagesPartViewProps = {
     imageId: string;
@@ -56,10 +51,6 @@ export function ImagesPartView({ imageId }:ImagesPartViewProps) {
   //const mutation = useMutation((image) => uploadImage(image))
   
   const onSubmit = async () => {
-<<<<<<< HEAD:src/features/constatations/subfeatures/images/components/ImagesPartView.tsx
-=======
-    //todo: pause
->>>>>>> ba2eafa75f8d6794619496d6b4b587a224df979b:src/features/constatations/components/PartsView/ImagesPartView.tsx
     await imageUploadMutation.mutateAsync({
         image: image,
         imageId: imageId,
@@ -75,42 +66,13 @@ export function ImagesPartView({ imageId }:ImagesPartViewProps) {
     });
     setImage(null);
   };
-  const onDeleteSubmit = async () => {
-    //todo: pause
-    await imageDeleteMutation.mutateAsync({
-        constatationId: imageQuery?.data?.constatation_id,
-        imageId: imageId,
-    });
-    setImage(null);
-  };  
 
   return(
-<<<<<<< HEAD:src/features/constatations/subfeatures/images/components/ImagesPartView.tsx
     <>
       <ScrollView >
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center", margin:"10px" }}>
           <Text h2>{imageQuery?.data?.name}</Text>
           <Button title="Supprimer l'image" onPress={onDeleteSubmit} />
-=======
-  <View>
-    <Text h1>{imageQuery?.data?.name}</Text>
-    <Button title="Supprimer l'image" onPress={onDeleteSubmit} />
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>  
-      {image && (
-        <Image
-          source={{ uri: image.uri }}
-          style={{ width: 300, height: 300 }}
-        />
-      )}
-    </View>
-
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-    <Button title="Prends une photo" onPress={pickImage} />
-    <Button title="Upload là" onPress={onSubmit} />
-    </View>
-
-  </View>
->>>>>>> ba2eafa75f8d6794619496d6b4b587a224df979b:src/features/constatations/components/PartsView/ImagesPartView.tsx
 
           <Button title="Pick an image from camera roll" onPress={pickImage} />
           {image && (
