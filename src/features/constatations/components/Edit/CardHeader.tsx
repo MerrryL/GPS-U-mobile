5c41f7830abf8461c29fa1ab433bef266d1eab30
Merry <amo168@gmail.com>
2021-09-28 23:37:51 +0200

    WIP: observers

--- src/features/constatations/components/Edit/CardHeader.tsx
+++ src/features/constatations/components/Edit/CardHeader.tsx
@@ -1,23 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import { Card, Switch, Icon, Button, Text, Input } from "react-native-elements";
-import { format } from 'date-fns'
+import { format } from 'date-fns';
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
 import { View } from "react-native";
-import thumbURL from "../../utils/ThumbURL";
 import { useNavigation } from "@react-navigation/native";
 import { useConstatation } from "../../hooks/useConstatation";
 import { useUpdateConstatation } from "../../hooks/useUpdateConstatation";
 import imageURL from "../../utils/ImageURL";
+import { AntDesign, FontAwesome, FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
 
 type ConstatationValues = {
-  comment: string;
+  description: string;
 };
 
 const schema = yup.object().shape({
-  comment: yup.string().required(),
+  description: yup.string().required(),
 });
 
 export function CardHeader({ constatationId }) {
@@ -25,7 +25,12 @@ export function CardHeader({ constatationId }) {
     constatationId: constatationId,
   });
 
+  const  [constatation, setConstatation] = useState({description: constatationQuery?.data?.description})
+
   const updateConstatationMutation = useUpdateConstatation();
+
+
+
 
   const {
     control,
@@ -36,6 +41,7 @@ export function CardHeader({ constatationId }) {
   });
 
   const onSubmit = async (values) => {
+    console.log("here", values);
     await updateConstatationMutation.mutateAsync({
       data: values,
       constatationId: constatationId,
@@ -43,50 +49,7 @@ export function CardHeader({ constatationId }) {
     //onSuccess();
   };
   return (
-    <>
-      <Card.Divider />
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "row",
-          alignItems: "flex-end",
-          height: "auto",
-          borderBottomWidth: 1,
-          padding: 10,
-        }}
-      >
-        {constatationQuery?.data?.isValidated ? (
-          constatationQuery?.data?.requiresValidation ? (
-            <Text style={{ flex: 0.5, height: "auto", marginBottom: 10 }}>
-              Validation possible depuis le
-              {constatationQuery?.data?.requiresValidationDate}
-            </Text>
-            
-          ) : (
-            <Text style={{ flex: 0.5, height: "auto", marginBottom: 10 }}>
-              Non soumis à approbation
-            </Text>
-          )
-        ) : (
-          <Text style={{ flex: 0.5, height: "auto", marginBottom: 10 }}>
-            Date de validation: {constatationQuery?.data?.validationDate}
-          </Text>
-        )}
-      </View>
-      <View
-        style={{
-          flex: 1,
-          flexDirection:'row',
-          alignItems: "flex-end",
-        }}
-      >
-        <Text style={{ flex: 0.5, marginBottom: 10 }}>
-          Création: {constatationQuery?.data?.created_at}
-        </Text>
-        <Text style={{ flex: 0.5, marginBottom: 10 }}>
-          Dernière mise à jour: {constatationQuery?.data?.updated_at}
-        </Text>
-      </View>
+    <>     
       <Card.Divider />
       <View
         style={{
@@ -104,32 +67,70 @@ export function CardHeader({ constatationId }) {
           style={{ width: 200, height: 200 }}
         />
         <View
-          style={{
-            flex: 1,
-            width: "60%",
-          }}
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          alignItems: "flex-end",
+          height: "auto",
+          padding: 10,
+          width: "60%",
+        }}
         >
-          <Controller
-            control={control}
-            rules={{
-              required: true,
-            }}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-                placeholder="commentaires"
-                multiline
-                numberOfLines={4}
-              />
-            )}
-            name="comment"
-            defaultValue={constatationQuery?.data?.comment}
-          />
-          <Text>{errors.comment?.message}</Text>
-          <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+          {constatationQuery?.data?.isValidated ? (
+            constatationQuery?.data?.requiresValidation ? (
+              <Text style={{ flex: 0.5, height: "auto", marginBottom: 10 }}>
+                Validation possible depuis le
+                {constatationQuery?.data?.requiresValidationDate}
+              </Text>
+              
+            ) : (
+              <Text style={{ flex: 0.5, height: "auto", marginBottom: 10 }}>
+                Non soumis à approbation
+              </Text>
+            )
+          ) : (
+            <Text style={{ flex: 0.5, height: "auto", marginBottom: 10 }}>
+              Date de validation: {constatationQuery?.data?.validationDate}
+            </Text>
+          )}
         </View>
+      </View>
+      <View>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={constatation?.description}
+              placeholder="Description"
+              multiline
+              numberOfLines={4}
+            />
+          )}
+          name="description"
+          //defaultValue={}
+        />
+        <Text>{errors.comment?.message}</Text>
+        <Button title="Enregistrer " onPress={() => handleSubmit(onSubmit)} icon={<AntDesign name="cloudupload" size={24} color="white" />} iconRight={true}  />
+
+      </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection:'row',
+          alignItems: "flex-end",
+        }}
+      >
+        <Text style={{ flex: 0.5, marginBottom: 10 }}>
+          Création: {constatationQuery?.data?.created_at}
+        </Text>
+        <Text style={{ flex: 0.5, marginBottom: 10 }}>
+          Dernière modification: {constatationQuery?.data?.updated_at}
+        </Text>
       </View>
 
       <Card.Divider />
