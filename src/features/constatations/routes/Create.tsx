b931627137a73b1bb0b00c3a914f27091175d99f
Merry <amo168@gmail.com>
2022-05-21 13:20:23 +0200

    fixes2

--- src/features/constatations/routes/Create.tsx
+++ src/features/constatations/routes/Create.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-
 import { Card } from "react-native-elements/dist/card/Card";
-import { ImagesPart } from "../subfeatures/images/components/ImagesPart";
-
-import { LocalizationPart } from "../subfeatures/localization/components/LocalizationPart";
+// import { ImagesPart } from "../subfeatures/images/components/ImagesPart";
+// import { LocalizationPart } from "../subfeatures/localization/components/LocalizationPart";
 import { useCreateConstatation } from "../hooks/useCreateConstatation";
+
 
 export default function Create() {
   const createConstatationMutation = useCreateConstatation();
 
   return (
     <Card>
-      <ImagesPart />
-      <LocationPart />
+      {/* <ImagesPart /> */}
+      {/* <LocationPart /> */}
     </Card>
   );
 }
